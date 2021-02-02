c9dc6f934a820624cc799e545ed4e470fb8cc2fa
Dan Brower <danielsbrower@gmail.com>
2021-02-02 02:33:12 -0800

    Changes to address first review

--- app.js
+++ app.js
@@ -1,121 +1,118 @@
-
 const Dinos = [
     {
-        "species": "Triceratops",
-        "weight": 13000,
-        "height": 114,
-        "diet": "herbavor",
-        "where": "North America",
-        "when": "Late Cretaceous",
-        "fact": "First discovered in 1889 by Othniel Charles Marsh"
+        'species': 'Triceratops',
+        'weight': 13000,
+        'height': 114,
+        'diet': 'herbavor',
+        'where': 'North America',
+        'when': 'Late Cretaceous',
+        'fact': 'First discovered in 1889 by Othniel Charles Marsh'
     },
     {
-        "species": "Tyrannosaurus Rex",
-        "weight": 11905,
-        "height": 144,
-        "diet": "carnivor",
-        "where": "North America",
-        "when": "Late Cretaceous",
-        "fact": "The largest known skull measures in at 5 feet long."
+        'species': 'Tyrannosaurus Rex',
+        'weight': 11905,
+        'height': 144,
+        'diet': 'carnivor',
+        'where': 'North America',
+        'when': 'Late Cretaceous',
+        'fact': 'The largest known skull measures in at 5 feet long.'
     },
     {
-        "species": "Anklyosaurus",
-        "weight": 10500,
-        "height": 55,
-        "diet": "herbavor",
-        "where": "North America",
-        "when": "Late Cretaceous",
-        "fact": "Anklyosaurus survived for approximately 135 million years."
+        'species': 'Anklyosaurus',
+        'weight': 10500,
+        'height': 55,
+        'diet': 'herbavor',
+        'where': 'North America',
+        'when': 'Late Cretaceous',
+        'fact': 'Anklyosaurus survived for approximately 135 million years.'
     },
     {
-        "species": "Brachiosaurus",
-        "weight": 70000,
-        "height": "372",
-        "diet": "herbavor",
-        "where": "North America",
-        "when": "Late Jurasic",
-        "fact": "An asteroid was named 9954 Brachiosaurus in 1991."
+        'species': 'Brachiosaurus',
+        'weight': 70000,
+        'height': '372',
+        'diet': 'herbavor',
+        'where': 'North America',
+        'when': 'Late Jurasic',
+        'fact': 'An asteroid was named 9954 Brachiosaurus in 1991.'
     },
     {
-        "species": "Stegosaurus",
-        "weight": 11600,
-        "height": 79,
-        "diet": "herbavor",
-        "where": "North America, Europe, Asia",
-        "when": "Late Jurasic to Early Cretaceous",
-        "fact": "The Stegosaurus had between 17 and 22 seperate places and flat spines."
+        'species': 'Stegosaurus',
+        'weight': 11600,
+        'height': 79,
+        'diet': 'herbavor',
+        'where': 'North America, Europe, Asia',
+        'when': 'Late Jurasic to Early Cretaceous',
+        'fact': 'The Stegosaurus had between 17 and 22 seperate places and flat spines.'
     },
     {
-        "species": "Elasmosaurus",
-        "weight": 16000,
-        "height": 59,
-        "diet": "carnivor",
-        "where": "North America",
-        "when": "Late Cretaceous",
-        "fact": "Elasmosaurus was a marine reptile first discovered in Kansas."
+        'species': 'Elasmosaurus',
+        'weight': 16000,
+        'height': 59,
+        'diet': 'carnivor',
+        'where': 'North America',
+        'when': 'Late Cretaceous',
+        'fact': 'Elasmosaurus was a marine reptile first discovered in Kansas.'
     },
     {
-        "species": "Pteranodon",
-        "weight": 44,
-        "height": 20,
-        "diet": "carnivor",
-        "where": "North America",
-        "when": "Late Cretaceous",
-        "fact": "Actually a flying reptile, the Pteranodon is not a dinosaur."
+        'species': 'Pteranodon',
+        'weight': 44,
+        'height': 20,
+        'diet': 'carnivor',
+        'where': 'North America',
+        'when': 'Late Cretaceous',
+        'fact': 'Actually a flying reptile, the Pteranodon is not a dinosaur.'
     },
     {
-        "species": "Pigeon",
-        "weight": 0.5,
-        "height": 9,
-        "diet": "herbavor",
-        "where": "World Wide",
-        "when": "Holocene",
-        "fact": "All birds are living dinosaurs."
+        'species': 'Pigeon',
+        'weight': 0.5,
+        'height': 9,
+        'diet': 'herbavor',
+        'where': 'World Wide',
+        'when': 'Holocene',
+        'fact': 'All birds are living dinosaurs.'
     }
 ]
 
 // Create Human Object
-let human = { height: 0, weight: 0, diet: "", name: "" };
+const human = { height: 0, weight: 0, diet: '', name: '' };
 
-// Create Dino Compare Method 1
+// Method to compare weight
 function cmpWeight( weight )
 {
-    let pct = Math.floor( ( weight / human.weight ) * 100 );
-    let str = pct + '% of your weight';
+    const pct = Math.floor( ( weight / human.weight ) * 100 );
+    const str = pct + '% of your weight';
     return str;
 }
 
-// Create Dino Compare Method 2
-// NOTE: Height in JSON file is in inches.
+// Method to compare height
 function cmpHeight( height )
 {
-    let pct = Math.floor( ( height / human.height ) * 100 );
-    let str = pct + '% of your height';
+    const pct = Math.floor( ( height / human.height ) * 100 );
+    const str = pct + '% of your height';
     return str;
 }
 
-// Create Dino Compare Method 3
+// Method to compare diet
 function cmpDiet( diet )
 {
-    let str = "";
-
     if( diet == human.diet ) {
-        str = 'Same as your diet!';
+        return 'Same as your diet!';
     }
     else {
-        str = 'Different than your diet';
+        return 'Different than your diet';
     }
-    return str;
 }
 
+// Method to get random fact
 function getFact( element ) {
             
     let fact;
+    
     if( element.species == 'Pigeon' ) {
         fact = element.fact;
     }
     else {
-        let rand = Math.floor(Math.random() * Math.floor(6))    // 0 to 5
+        const rand = Math.floor(Math.random() * Math.floor(6))    // 0 to 5
         switch( rand ) {
             case 0:
                 fact = element.fact;
@@ -140,7 +137,7 @@ function getFact( element ) {
     return fact;
 }
 
-// Create Dino Constructor
+// Dino Constructor
 function Dino( obj ) {
     this.species = obj.species;
     this.weight = obj.weight;
@@ -151,14 +148,45 @@ function Dino( obj ) {
     this.fact = obj.fact;
 }
 
+// Method to generate tile for display
+function genTile(name, image, fact, center) { 
+
+    const tile = document.createElement('div');
+    tile.className = 'grid-item';
+
+    const h3 = document.createElement('h3');
+    h3.innerHTML = name;
+    tile.appendChild( h3 );
+
+    const img = document.createElement('img');
+    img.src = './images/' + image + '.png';
+    tile.appendChild( img );
+
+    if( fact != '' ) {
+        const p = document.createElement('p');
+        p.innerHTML = fact;
+        tile.appendChild( p );
+    }
+
+    // Add tiles to DOM
+    if(center) {
+        const parent = document.getElementById('grid');
+        parent.insertBefore(tile, parent.children[4]);
+    }
+    else {
+        document.getElementById('grid').appendChild( tile );
+    }
+}
+
+// Method to handle form submit
 function dspInfo() {
 
-    let dinos = [];
+    const dinos = [];
      
     // Create Dino Objects
     Dinos.forEach( element => dinos.push( new Dino( element ) ) );
 
-    // Use IIFE to get human data from form
+    // Get human data from form
     (function() {
         human.weight = document.getElementById('weight').value;
         human.height = Number(document.getElementById('feet').value) * 12 + 
@@ -169,46 +197,15 @@ function dspInfo() {
 
     // Generate Tiles for each Dino in Array
     dinos.forEach(element => {
-        let tile = document.createElement('div');
-        tile.className = 'grid-item';
-
-        let h3 = document.createElement('h3');
-        h3.innerHTML = element.species;
-        tile.appendChild( h3 );
-
-        let img = document.createElement('img');
-        img.src = './images/' + element.species + '.png';
-        tile.appendChild( img );
-
-        // get fact
-        let fact = getFact( element );
-        let p = document.createElement('p');
-        p.innerHTML = fact;
-        tile.appendChild( p );
-
-        // Add tiles to DOM
-        document.getElementById("grid").appendChild( tile );
+        genTile( element.species, element.species, getFact(element), false );
     })
 
     // Generate human tile
-    let tile = document.createElement('div');
-    tile.className = 'grid-item';
+    genTile( human.name, 'human', '', true);
 
-    let h3 = document.createElement('h3');
-    h3.innerHTML = human.name;
-    tile.appendChild( h3 );
-
-    let img = document.createElement('img');
-    img.src = './images/' + 'human' + '.png';
-    tile.appendChild( img );
-
-    // Add tile to DOM
-    let parent = document.getElementById("grid");
-    parent.insertBefore(tile, parent.children[4]);
-
-    // Remove form from screen
-    document.getElementById("dino-compare").hidden = true;
+     // Remove form from screen
+    document.getElementById('dino-compare').hidden = true;
 }
 
-// On button click, prepare and display infographic
+// Setup button click handler
 document.getElementById('btn').addEventListener('click', dspInfo );
